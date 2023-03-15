78336f3ea232938dfbb10654bedf4db28c46481a
Cristian Torres Salazar <tor8@pdx.edu>
2023-03-15 16:46:52 -0700

    Progress: Completes initial index page

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
@@ -20,15 +21,90 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geo = new THREE.TorusGeometry(10, 3, 16, 100);
+// create first shape
+
+// new chape geometry
+const geo = new THREE.TorusKnotGeometry(5, 2, 100, 8, 2, 3);
+const geo2 = new THREE.TorusKnotGeometry(5, 2, 100, 8, 2, 3);
+const geo3 = new THREE.TorusKnotGeometry(10, 2, 100, 8, 2, 3);
+const geo4 = new THREE.TorusKnotGeometry(10, 3, 64, 8, 2, 3);
+const geo5 = new THREE.TorusKnotGeometry(10, 3, 64, 8, 2, 3);
+const geo6 = new THREE.TorusKnotGeometry(10, 3, 64, 8, 2, 3);
+const geo7 = new THREE.TorusKnotGeometry(10, 3, 64, 8, 2, 3);
+const geo8 = new THREE.TorusKnotGeometry(10, 3, 64, 8, 2, 3);
+const geo9 = new THREE.TorusKnotGeometry(10, 3, 64, 8, 2, 3);
+
+// create material for shape
 const material = new THREE.MeshStandardMaterial({
-  color: 0x007575,
+  color: 0xd60047,
   wireframe: true,
 });
 
+// create torus geometry with material
 const geoTorus = new THREE.Mesh(geo, material);
+const geoTorus2 = new THREE.Mesh(geo2, material);
+const geoTorus3 = new THREE.Mesh(geo3, material);
+const geoTorus4 = new THREE.Mesh(geo4, material);
+const geoTorus5 = new THREE.Mesh(geo5, material);
+const geoTorus6 = new THREE.Mesh(geo6, material);
+const geoTorus7 = new THREE.Mesh(geo7, material);
+const geoTorus8 = new THREE.Mesh(geo8, material);
+const geoTorus9 = new THREE.Mesh(geo9, material);
 
-scene.add(geoTorus);
+const SetPositionParameters = (geo, pX, pY, pZ, rX, rY, rZ) => {
+  geo.position.x = pX;
+  geo.position.y = pY;
+  geo.position.z = pZ;
+  geo.rotation.x += rX;
+  geo.rotation.y += rY;
+  geo.rotation.z += rZ;
+};
+
+const GetRandomVal = (neg) => {
+  let val = Math.floor(Math.random() * 300) + 1;
+
+  if (neg === true) {
+    return val * -1;
+  }
+  return val;
+};
+
+const AutomateGeos = () => {
+  let neg = false;
+  for (let i = 0; i < 200; ++i) {
+    if (i % 2 === 0) {
+      neg = true;
+    } else {
+      neg = false;
+    }
+
+    const newGeo = new THREE.TorusKnotGeometry(10, 3, 64, 8, 2, 3);
+    const newGeoTorus = new THREE.Mesh(newGeo, material);
+    SetPositionParameters(
+      newGeoTorus,
+      GetRandomVal(neg),
+      GetRandomVal(neg),
+      GetRandomVal(neg),
+      0.01,
+      0.005,
+      0.01
+    );
+    scene.add(newGeoTorus);
+  }
+};
+
+scene.add(
+  geoTorus,
+  geoTorus2,
+  geoTorus3,
+  geoTorus4,
+  geoTorus5,
+  geoTorus6,
+  geoTorus7,
+  geoTorus8,
+  geoTorus9
+);
+//scene.add(geoTorus2);
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(20, 20, 20);
@@ -40,16 +116,27 @@ const gridHelper = new THREE.GridHelper(200, 50);
 const controls = new OrbitControls(camera, renderer.domElement);
 scene.add(gridHelper);
 
-const spaceBack = new THREE.TextureLoader().load('./images/network.jpg');
+const spaceBack = new THREE.TextureLoader().load(
+  './images/backimages/network.jpg'
+);
 scene.background = spaceBack;
 
 function animate() {
   requestAnimationFrame(animate);
-  geoTorus.rotation.x += 0.01;
-  geoTorus.rotation.y += 0.005;
-  geoTorus.rotation.z += 0.01;
+
+  SetPositionParameters(geoTorus, 10, 10, 0, 0.01, 0.005, 0.01);
+  SetPositionParameters(geoTorus2, -25, 0, 5, 0.01, 0.005, 0.01);
+  SetPositionParameters(geoTorus3, 0, -20, 20, 0.01, 0.005, 0.01);
+  SetPositionParameters(geoTorus4, 0, 0, 50, 0.01, 0.005, 0.01);
+  SetPositionParameters(geoTorus5, -40, 20, 50, 0.01, 0.005, 0.01);
+  SetPositionParameters(geoTorus6, 40, 0, 45, 0.01, 0.005, 0.01);
+  SetPositionParameters(geoTorus7, 40, -35, 75, 0.01, 0.05, 0.01);
+  SetPositionParameters(geoTorus8, -50, -45, 90, 0.01, 0.005, 0.01);
+  SetPositionParameters(geoTorus9, -40, -20, 140, 0.01, 0.005, 0.01);
+
   controls.update();
   renderer.render(scene, camera);
 }
 
+AutomateGeos();
 animate();
